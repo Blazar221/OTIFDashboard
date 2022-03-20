da8ada64770641a1e05cce66f705031329ca2d6f
Blazar <805781038@qq.com>
2022-03-19 17:50:51 -0700

    add table header

--- src/app/pages/OTIFTable.tsx
+++ src/app/pages/OTIFTable.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
 
-export function OTIFTable() {
-  return <h1>OTIF Table</h1>
+export class OTIFTable extends React.Component<any, any> {
+
+  render() {
+    return <div>
+      <div className='input-container'>
+        <input
+          type='text'
+          className='form-control'
+          style={{width: '320px'}}
+          placeholder='Numero de rastreo'
+        />
+        <a href='#' className='btn btn-input'>Rastrer</a>
+        <input
+          type='date'
+          className='form-control'
+          style={{width: '180px', marginLeft: 'auto', marginRight: '10px', position: 'relative', top: '10px'}}
+        />
+      </div>
+    </div>
+  }
 }
