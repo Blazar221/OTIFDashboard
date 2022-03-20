d8b3ce0832019c160a475713ce776c63ff889064
Blazar <805781038@qq.com>
2022-03-20 15:37:21 -0700

    finish header

--- src/app/modules/otif/OtifHeader.tsx
+++ src/app/modules/otif/OtifHeader.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import {usePageData} from '../../../_metronic/layout/core'
+import {ReactComponent as SearchIcon} from './search.svg'
+import {ReactComponent as Down} from './pull-down.svg'
 
 function OtifHeader() {
-  const {pageTitle, pageDescription, pageBreadcrumbs} = usePageData()
+  const {pageTitle} = usePageData()
   return (
-    <div>
-      {pageTitle}
+    <div style={{padding: '16px', display: 'flex', alignItems: 'center', justifyContent: 'flex-end'}}>
+      <div style={{fontSize: '35px', marginRight: 'auto', textAlign: 'start'}}>
+        {pageTitle}
+      </div>
+      <div style={{background: '#fafafa', borderRadius: '8px', overflow: 'hidden', padding: '10px', margin: '0 5px 0 5px'}}>
+        <input type='text' placeholder='search' style={{background: '#fafafa', outline: 0, border: 0}} />
+        <SearchIcon style={{width: '20px', height: '20px'}} />
+      </div>
+      <img src='avator.png' style={{height: '35px'}} />
+      <p style={{margin: '0 5px 0 5px'}}>Jessica Jones</p>
+      <Down style={{height: '20px', width: '20px'}} />
     </div>
   )
 }
