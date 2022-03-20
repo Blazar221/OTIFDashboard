2a3423139871ef8d01ee0f44dab0977d84d65c4d
Blazar <805781038@qq.com>
2022-03-19 20:59:25 -0700

    finish table

--- src/app/pages/OTIFTable.tsx
+++ src/app/pages/OTIFTable.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import {ReactComponent as CircleGreen} from './circle-green.svg'
+import {ReactComponent as CircleBlue} from './circle-blue.svg'
+import {ReactComponent as CircleGray} from './circle-gray.svg'
+import {ReactComponent as Down} from './pull-down.svg'
 
 export class OTIFTable extends React.Component<any, any> {
 
   render() {
     const data = [
-      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Entregado'],
-      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'En Transito'],
-      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Entregado'],
-      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'En Transito'],
-      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Salio de Cento'],
-      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Entregado'],
-      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Entregado'],
-      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Entregado'],
-      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Entregado'],
-      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Entregado'],
+      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Entregado', '0'],
+      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'En Transito', '1'],
+      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Entregado', '0'],
+      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'En Transito', '1'],
+      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Salio de Cento', '2'],
+      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Entregado', '1'],
+      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Entregado', '1'],
+      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Entregado', '1'],
+      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Entregado', '1'],
+      ['00986455', 'OC00995', 'Monterrey', 'Oaxaca', 'Redpack', '11/03/2022', '12/03/2022', 'Entregado', '1'],
     ]
 
     return <div>
@@ -32,21 +36,19 @@ export class OTIFTable extends React.Component<any, any> {
         />
       </div>
       <div className='ra-table'>
-        <div className='ra-header-container'>
-          <div className='ra-header'>
-            <p>No. of Referencia</p>
-            <p>No. of Rastreo</p>
-            <p>Origen</p>
-            <p>Destino</p>
-            <p>Paqueteria</p>
-            <p>Fecha Recoleccion</p>
-            <p>Fecha De Entrega</p>
-            <p>Estatus</p>
-          </div>
+        <div className='ra-line' id='ra-header'>
+          <p style={{width: "17%"}}>No. of Referencia</p>
+          <p>No. of Rastreo</p>
+          <p style={{width: "12%"}}>Origen</p>
+          <p>Destino</p>
+          <p>Paqueteria</p>
+          <p style={{width: "18%"}}>Fecha Recoleccion</p>
+          <p style={{width: "18%"}}>Fecha De Entrega</p>
+          <p style={{width: "18%"}}>Estatus</p>
         </div>
         <div>
           {data.map((item, i) => {
-            return (<div className='ra-line' key={i}>
+            return <div className='ra-line' key={i}>
               <p>{item[0]}</p>
               <p>{item[1]}</p>
               <p>{item[2]}</p>
@@ -54,10 +56,19 @@ export class OTIFTable extends React.Component<any, any> {
               <p>{item[4]}</p>
               <p>{item[5]}</p>
               <p>{item[6]}</p>
-              <p>{item[7]}</p>
-            </div>)
+              <p style={{width: "18%"}}>
+                {(item[8] === '0') ? <CircleGreen style={{width: '10px', height: '10px'}} /> :
+                  (item[8] === '1') ? <CircleBlue style={{width: '10px', height: '10px'}} /> :
+                    <CircleGray style={{width: '10px', height: '10px'}} />
+                }
+                {item[7]}<Down style={{width: '14px', height: '14px'}} />
+              </p>
+            </div>
           })}
         </div>
+      </div>
+      <div style={{display: 'flex'}}>
+        <a href='#' className='btn-download'>Download stats</a>
       </div>
     </div>
   }
